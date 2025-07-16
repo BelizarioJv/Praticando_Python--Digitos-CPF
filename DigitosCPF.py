893bfd530d09f73c6e2b5d06d4dcebac8409f537
João victor Belizario dos santos <113644605+BelizarioJv@users.noreply.github.com>
2025-07-16 15:46:23 -0300

    Update DigitosCPF.py

--- DigitosCPF.py
+++ DigitosCPF.py
@@ -1,14 +1,15 @@
-# Primeiro Digito
-cpf = '70037622692'
+import random
+numero_cpf = ''
+for index in range (0,9):
+  numero_cpf += str(random.randint(0,9))
+print(f'Os nove numeros do cpf e {numero_cpf}')
+
 i = 0
 primeiroDigito = 0
 for numero in range(10,1,-1):
-  primeiroDigito += int(cpf[i]) * numero
+  primeiroDigito += int(numero_cpf[i]) * numero
   i +=1
-  print(primeiroDigito)
   
-#       Segundo Digito
-
 primeiroDigito *= 10 
 primeiroDigito = primeiroDigito % 11
 if primeiroDigito > 9 :
@@ -16,19 +17,14 @@ if primeiroDigito > 9 :
 else:
   print(f'O primeiro digito e {primeiroDigito}')
 
-cpf = '70037622692'
-i = 0
-segundoDigito = 0
-for numero in range(11,1,-1):
-  segundoDigito += int(cpf[i]) * numero
-  i +=1
-  print(segundoDigito)
-  
+cpf_com_primeiro = numero_cpf + str(primeiroDigito)
 
-segundoDigito *= 10 
-segundoDigito = segundoDigito % 11
-if segundoDigito > 9 :
-  print('O digito e 0')
-else:
+soma = sum(int(cpf_com_primeiro[i]) * (11 - i) for i in range(10))
+segundoDigito = (soma * 10) % 11
+if segundoDigito > 9:
+    segundoDigito = 0
+print(f'O segundo dígito é {segundoDigito}')
+
+print(f'O cpf gerado e:{numero_cpf[0:3]}.{numero_cpf[3:6]}.{numero_cpf[6:9]}-{primeiroDigito}{segundoDigito}')
   print(f'O segundo digito e {segundoDigito}')
 
